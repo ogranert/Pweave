48b814953bdbf3bc07edd851920a2a51e045f7e1
olg <olg@localhost.localdomain>
2024-10-06 12:12:04 +0200

    some more info about the markdown math priority

--- pweave/formatters/markdownmath.py
+++ pweave/formatters/markdownmath.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import Element
 import markdown
 
 
@@ -20,5 +21,8 @@ class MathExtension(markdown.Extension):
 #    def extendMarkdown(self, md, md_globals):
 #        md.inlinePatterns.add('math', MathPattern(), '<escape')
     def extendMarkdown(self, md):
+		# An extension need to tell Markdown about them and ensure that they are run in the proper sequence.
+		# We should give "math" a higher priority (185) than 'escape' which has 180 and also begin/end pattern should have lower priority than all others.
+		# Links to more info: https://python-markdown.github.io/extensions/api/ and https://github.com/mitya57/python-markdown-math/blob/master/mdx_math.py
         md.inlinePatterns.register(MathPattern(), 'math', 185)
         
